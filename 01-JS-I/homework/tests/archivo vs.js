2e5755e2cc43767a563ee20b2dd72acc61a4fb9f
esmeralda <esmeg1104@gmail.com>
2022-09-18 22:29:03 -0500

    hw 01

---
+++ 01-JS-I/homework/tests/archivo vs.js
@@ -0,0 +1,10 @@
+var nombre= "davxter";
+
+ console.log (nombre);
+
+ var pais = "colombia";
+
+var nombregato = "morocha";
+
+var yojodo = false;
+
--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,34 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  
 }
   
   
